f9f6a0b80e2354395d6125c7b6131a406b56ff12
George Jacob <georgej@datacom.co.nz>
2014-09-21 23:22:51 +1200

    Commit5 (+1 squashed commits)
    Squashed commits:Squahed 4 &5
    
    [2f2d988] Commit4

--- GitTesting/GitTesting/Program.cs
+++ GitTesting/GitTesting/Program.cs
@@ -13,6 +13,8 @@ namespace GitTesting
 			Console.WriteLine("Commit1");
 			Console.WriteLine("Commit2");
 			Console.WriteLine("Commit3");
+			Console.WriteLine("Commit4");
+			Console.WriteLine("Commit5");
 		}
 	}
 }
